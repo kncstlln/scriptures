3793642fab3c475f79d518eefb70301266cc6c01
Kane Castillano <castillano.kaneerryl@auf.edu.ph>
2022-10-22 23:07:29 +0800

    second commit

--- books.php
+++ books.php
@@ -1,28 +1,35 @@
 <?php
-include "vendor/autoload.php";
+
+require "vendor/autoload.php";
+
 use GuzzleHttp\Client;
 
-
 function getBooks() {
-$token='bf04c1fce3de88d595fd160758d362ddbf3a58e292164d8fdf0892bcf5002e85e68e543fe34503581e2b28f2c86dbd110c9ef3e0e5e1186fb93f18daa2bd3a361cee57ca15e847225382ff8578f8660fd2e083c98df972eb8161bd72a31ced825c56ef697628cb235488c7d513ebdb6a1d88aafbb509a509cb3a525a1d65816a';
-$client = new Client([
-    'base_uri'=>'http://localhost:1337/api/'
-]);
-$headers = [
-  'Authorization' => 'Bearer ' . $token,
-  'Accept' => 'application/json'
-];
+    $token = 'aedeec30db6207585290f2b091b4a59eb50cc4fdca5b3feb763245c2458289f3eaee469c3d1a7de59a22375753ac297d1cc17a5b8a6da31526da2b39e02121cf0a36c4c2f62368d7f6408360f337256ba4799f283d6a1b53a4c66e5b5daf59e7c7c5a42de54d5308d25ec96f13cc154c4651634b35c7a182538213fed62f8d3f';
 
-$response = $client->request('GET', 'bibles', ['headers' => $headers]);
-$body = $response->getBody();
-var_dump (json_decode($body));
+    $client = new Client([
+        'base_uri' => 'http://localhost:1337/api/',
+    ]);
+
+    $headers = [
+        'Authorization' => 'Bearer ' . $token,
+        'Accept'        => 'application/json',
+    ];
+
+    $response = $client->request('GET', 'books?pagination[pageSize]=66', [
+        'headers' => $headers
+    ]);
+
+    $body = $response->getBody();
+    $decoded_response = json_decode($body);
+    return $decoded_response;
 }
 
 $books = getBooks();
 
 ?>
 <hmtl>
-    
+
     <title> IPT10 </title>
     <h1> IPT10 Scriptures </h1>
     <h2><a href = "#"><u> Castillano, Kane Erryl G. </u></a></h2>
@@ -40,10 +47,8 @@ $books = getBooks();
   </thead>
   <tbody>
     <?php
-    foreach ($books->data as $bookList){
-        $book = $bookList->attributes;
-
-    ?>
+     foreach($books->data as $bookData){ 
+        $book = $bookData->attributes;?>
     <tr>
       <td><?=$bookData->id?></td>
       <td><?=$book->name?></td>
@@ -55,8 +60,3 @@ $books = getBooks();
 </table>
 </body>
 </html>
-
-
-
-
-
